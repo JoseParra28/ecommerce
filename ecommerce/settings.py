38a3e0d83177541e8e89f5ea4be3c3717c49b84d
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-09-18 21:33:17 +0100

    static
    created static files, media root , ran migrations

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,6 +119,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILE_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'static/media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
