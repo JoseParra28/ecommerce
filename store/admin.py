4e897784ce7d6b62549c247ff930876364fa53a5
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-09-18 21:50:46 +0100

    Models
    set up models functions

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Cetegory, Product
+
+
+@admin.register(Cetegory)
+class CetegoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
