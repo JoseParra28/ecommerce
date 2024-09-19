fd44272842dcd35c0c0810bc9616f4159cba06b8
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-09-19 11:37:52 +0100

    Categories
    Added products to the database, setup a dropdown with all categories

--- store/views.py
+++ store/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from . models import Cetegory
 # Create your views here.
+
+
 def store (request):
     return render(request, 'store/store.html')
+
+def categories(request):
+
+    all_categories = Cetegory.objects.all()
+
+    return{'all_categories': all_categories}
